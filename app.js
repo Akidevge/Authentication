ff1d9dce84fbacd95ba6a012a58a2fca4d76e2d8
Akidevge <zurajanai737@gmail.com>
2023-06-28 22:17:08 +0530

    Added Google OAuth

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const ejs = require("ejs");
 const session = require('express-session')
 const passport = require("passport")
 const passportLocalMongoose = require("passport-local-mongoose")
+const findOrCreate = require('mongoose-findorcreate')
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -24,16 +26,50 @@ app.use(passport.session());
 mongoose.connect("mongodb://127.0.0.1:27017/userDB");
 const userSchema = new mongoose.Schema({
   email:String,
-  password:String
+  password:String,
+  googleId:String
 });
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 const User = mongoose.model("User",userSchema);
 passport.use(User.createStrategy());
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
-app.get("/home",(req,res)=>{
+passport.serializeUser(function(user, cb) {
+  process.nextTick(function() {
+    cb(null, { id: user.id, username: user.username, name: user.name });
+  });
+});
+
+passport.deserializeUser(function(user, cb) {
+  process.nextTick(function() {
+    return cb(null, user);
+  });
+});
+passport.use(new GoogleStrategy({
+  clientID: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  callbackURL: "http://localhost:3000/auth/google/secrets",
+  userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+},
+function(accessToken, refreshToken, profile, cb) {
+  console.log(profile);
+
+  User.findOrCreate({ googleId: profile.id }, function (err, user) {
+    return cb(err, user);
+  });
+}
+));
+app.get("/",(req,res)=>{
     res.render("home");
 });
+app.get("/auth/google",
+  passport.authenticate('google', { scope: ["profile"] })
+);
+app.get('/auth/google/secrets', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/secrets');
+  });
 app.get("/login",(req,res)=>{
   res.render("login");
 });
@@ -43,7 +79,7 @@ app.get("/logout",(req,res)=>{
       console.log(err);
      }
      else{
-      res.redirect('/home');
+      res.redirect('/');
      }
   });
 });
